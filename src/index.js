800b6dfa5aa378986633b7f2437c3861bba0bba2
GSimic22 <112600678+GSimic22@users.noreply.github.com>
2022-09-19 11:29:45 -0400

    adding future forecast

--- src/index.js
+++ src/index.js
@@ -21,34 +21,41 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+
+function formatDay(timestamp) {
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+return day[day];
+}
 function displayForecast(response){
-  console.log(response.data.daily);
+  let forecast = (response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class ="row">`;
   let days = ["Monday", "Tuesday", "Wednesday","Thursday"];
-  days.forEach(function(day){
-    forecastHTML = 
-    forecastHTML +
-    `
+  forecast.forEach(function(forecastDay, index){
+    if (index < 6) {
+    forecastHTML =
+      forecastHTML +
+      `
  <div class="col-2">
-    <div class="weather-forecast-date"> 
-    ${day}
+    <div class="weather-forecast-date"${formatDay(forecastDay.dt)}>
     </div>
-    <img src="http://openweathermap.org/img/wn/04d@2x.png"
+    <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
     alt=""
     width="42"
     />
     <div class ="weather-forecast-temp">
     <span class = "weather-forecast-temp-max">
-      18°
+      ${Math.round(forecastDay.temp.max)}°
       </span> 
       <span class = "weather-forecast-temp-min">
-        12°
+         ${Math.round(forecastDay.temp.min)}°
       </span>
     </div>
   </div>
-`;
+`;}
   });
 
 forecastHTML = forecastHTML + `</div>`;
